66de962fa403a30a8c9ae9ac257c15b355a6eeb2
HiiWee <wpdlzhf159@gmail.com>
2022-12-06 15:46:19 +0900

    feat: 영수증 페이지 테이블 형식 이용

---
+++ pos/src/main/java/com/project/service/BillCheckService.java
@@ -0,0 +1,9 @@
+package com.project.service;
+
+import com.project.repository.SellProductRepository;
+
+public class BillCheckService {
+    private final SellProductRepository sellProductRepository = SellProductRepository.getInstance();
+
+
+}
